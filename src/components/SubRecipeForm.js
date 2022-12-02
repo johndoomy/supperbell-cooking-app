f9757fe98c6b2e8cd7b31c56190afd825d774f16
johndoomy <johndoomyjr@gmail.com>
2022-12-01 22:31:49 -0700

    sub recipe form progress

--- src/components/SubRecipeForm.js
+++ src/components/SubRecipeForm.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react"
+
 const SubRecipeForm = () => {
+    const [arrayLength, setArrayLength] = useState(1)
+    const handleClick = () => {
+        let newLength = arrayLength + 1
+        setArrayLength(newLength)
+        console.log(newLength)
+    }
+    let array = [...Array(arrayLength)].map((_, i) => i)
+    console.log(array)
   return (
-    <div>
-        SubRecipe Form Here
+    <div className="formContainer subRecipeForm">
+        <form action="">
+            <p>Sub Recipe:</p>
+            {array.map((item, index) => <input key={index} className="subRecipeIngredientInput" type="text" name="" id="" />)} 
+            
+            <input onClick={handleClick} type="button" value="Add" />
+            
+        </form>
     </div>
   )
 }
