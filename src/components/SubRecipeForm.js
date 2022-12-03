a40a48901c0edd4237d6049f1ac0cac9d5e3c625
johndoomy <johndoomyjr@gmail.com>
2022-12-03 12:04:49 -0700

    amount and unit inputs to subR form

--- src/components/SubRecipeForm.js
+++ src/components/SubRecipeForm.js
@@ -7,27 +7,28 @@ const SubRecipeForm = () => {
         const name = event.target.name
         const value = event.target.value
         setInputs(values => ({...values, [name]: value}))
-        console.log(inputs)
     }
 
-    // const handleSubmit = event => {
-    //     event.preventDefault()
-    //     inputs.key = new Date().valueOf()
-    // }
+    const handleSubmit = event => {
+        event.preventDefault()
+        inputs.key = new Date().valueOf()
+        console.log(inputs)
+    }
 
     const [arrayLength, setArrayLength] = useState(1)
 
     const handleClick = () => {
         let newLength = arrayLength + 1
         setArrayLength(newLength)
+        
     }
 
     let array = [...Array(arrayLength)].map((_, i) => i)
     
   return (
     <div className="formContainer subRecipeForm">
-        <form action="">
-            <p>Sub Recipe:</p>
+        <form onSubmit={handleSubmit}>
+            <p>Sub Recipe Ingredients:</p>
             {array.map((item, index) => 
                 <div>
                     <input 
@@ -37,16 +38,27 @@ const SubRecipeForm = () => {
                         name={`ingredientName${index}`}
                         onChange={handleChange}
                     />
+                    <input onChange={handleChange} name={`ingredientAmount${index}`} size={3} type="tel" placeholder="#" />
+                    <select onChange={handleChange} name={`ingredientUnit${index}`} id="">
+                        <option value="each">each</option>
+                        <option value="c">cups</option>
+                        <option value="qt">quarts</option>
+                        <option value="lb">pounds</option>
+                        <option value="fl oz">fl oz (volume)</option>
+                        <option value="oz">oz (weight)</option>
+                        <option value="tbsp">tablespoons</option>
+                    </select>
                 </div>
             )}
 
-            <input onClick={handleClick} type="button" value="Add" />
+            <input onClick={handleClick} type="button" value="Add Ingredient" />
 
             <div>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <p>Directions:</p>
+                <textarea name="" id="" cols="46" rows="10"></textarea>
             </div>
             
-            
+            <input type="submit" value="Create Sub Recipe" />
         </form>
     </div>
   )
