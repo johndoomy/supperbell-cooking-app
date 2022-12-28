f71610e5d784727f053818aaf687906b059f1472
johndoomy <johndoomyjr@gmail.com>
2022-12-28 11:43:31 -0700

    updated to yarn

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AWS from 'aws-sdk'
-
-
-
+import AWS from 'aws-sdk';
 
 const credentials = new AWS.Credentials({
   accessKeyId: 'AKIA4APAIKIKCWEFIHHT',
-  secretAccessKey: '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'
+  secretAccessKey: '8H+ET5PxKh9xZZxfOVfLSkTG7DrZ3/eKRqnkKzjb',
 });
 
 // const options = {
@@ -18,16 +15,15 @@ const credentials = new AWS.Credentials({
 //   region: "us-east-2"
 // };
 
-
 AWS.config.update({
-  region: "us-east-2",
+  region: 'us-east-2',
   credentials: credentials,
   maxRetries: 2,
   httpOptions: {
     timeout: 2 * 1000,
     connectTimeout: 3 * 1000,
   },
-})
+});
 
 // const docClient = new AWS.DynamoDB.DocumentClient(options)
 // const docClient = new AWS.DynamoDB()
@@ -35,9 +31,6 @@ AWS.config.update({
 // console.log(AWS.config)
 
 // const dd = new AWS.DynamoDB()
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
