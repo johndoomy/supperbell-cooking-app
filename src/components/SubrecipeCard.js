b75492fff38ae1cc1a6b2782fe3643c2f46fb051
johndoomy <johndoomyjr@gmail.com>
2023-03-13 18:25:16 -0400

    update from mac

---
+++ src/components/SubrecipeCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubrecipeCard = ({ subRecipe }) => {
+  return (
+    <div className='card'>
+        <p>RECIPE HERE</p>
+    </div>
+  )
+}
+
+export default SubrecipeCard
